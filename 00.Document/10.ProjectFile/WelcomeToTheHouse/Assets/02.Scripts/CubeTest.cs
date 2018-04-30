ac8092733a11dab307f1ccd28ee8a27b629715de
YapHeo <ahrl1213@gmail.com>
2018-04-30 20:48:32 +0900

    [허지훈]인벤토리

---
+++ 00.Document/10.ProjectFile/WelcomeToTheHouse/Assets/02.Scripts/CubeTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTest : Item
+{
+    void Start()
+    {
+        base.InteractionId = 0;
+    }
+
+    void Update()
+    {
+
+    }
+}
--- 00.Document/10.ProjectFile/WelcomeToTheHouse/Assets/02.Scripts/Player.cs
+++ 00.Document/10.ProjectFile/WelcomeToTheHouse/Assets/02.Scripts/Player.cs
@@ -38,7 +38,7 @@ public class Player : MonoBehaviour
     {
         Cameracenter = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
 
-        if(SceneManager.GetActiveScene().name == "Stage1")
+        if(SceneManager.GetActiveScene().name == "Stage1" || SceneManager.GetActiveScene().name == "TempJH")
         {
             inven = Instantiate(inventory) as GameObject;
             inven.transform.position = new Vector3(Cameracenter.x, Cameracenter.y, -1);
@@ -87,12 +87,14 @@ public class Player : MonoBehaviour
                     inven.transform.position = new Vector3(hitcoll.collider.gameObject.transform.position.x, hitcoll.collider.gameObject.transform.position.y, hitcoll.collider.gameObject.transform.position.z);
                 }
 
+                // 인벤에 들어가는거
                 if(hitcoll.collider.CompareTag("InvenItem"))
                 {
                     inven.GetComponent<Inventory>().AddItem(hitcoll.collider.GetComponent<Item>().GetSpriteId());
                     Destroy(hitcoll.collider.gameObject);
                 }
 
+                // 인벤 닫기
                 if (hitcoll.collider.CompareTag("InvenClose"))
                 {
                     inven.transform.position = new Vector3(Cameracenter.x, Cameracenter.y, -1);
