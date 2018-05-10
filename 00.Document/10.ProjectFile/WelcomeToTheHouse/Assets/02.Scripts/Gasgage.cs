ca093192f46e7a69c346cbef7bbe6efa3c22e026
woojungyun <38061987+woojungyun@users.noreply.github.com>
2018-05-10 17:01:57 +0900

    0510
    가스게이지 bgm 넣음

--- 00.Document/10.ProjectFile/WelcomeToTheHouse/Assets/02.Scripts/Gasgage.cs
+++ 00.Document/10.ProjectFile/WelcomeToTheHouse/Assets/02.Scripts/Gasgage.cs
@@ -5,19 +5,60 @@ using UnityEngine.SceneManagement;
 public class Gasgage : MonoBehaviour
 {
     [SerializeField]
-    Image gage;
-
+    GameObject gasBar;
     [SerializeField]
-    Image progressbar;
+    GameObject gasgageBar;
 
+    Image progressbarImg;
+
+    float redValue;
+    float greenValue;
 
     float timeloss = 0.0033f;
 
+    float interval = 1f;
+    float ttime = 0;
+
+    string sceneName = "";
+
+    bool soundSW = true;
+
+    void Start()
+    {
+        progressbarImg = gasgageBar.GetComponent<Image>();
+    }
+
+
     void Update()
     {
-        progressbar.fillAmount += Time.deltaTime * timeloss;
+        sceneName = Application.loadedLevelName;
 
-        if (progressbar.fillAmount == 1 )
+        if (sceneName == "Stage1")
+        {
+            gasBar.SetActive(true);
+            gasgageBar.SetActive(true);
+
+            progressbarImg.fillAmount += Time.deltaTime * timeloss;
+        }
+
+        //#1 색을 부드럽게 변경 + 심장소리 재생
+        if (progressbarImg.fillAmount >= 0.8f)
+        {
+            redValue = Mathf.PingPong(Time.time * 150, 150);
+            greenValue = 150 - Mathf.PingPong(Time.time * 150, 150);
+
+            progressbarImg.color = new Color(redValue / 255, greenValue / 255, 3 / 255);
+
+            if (soundSW)
+            {
+                GetComponent<AudioSource>().Play();
+                soundSW = false;
+            }
+
+        }
+
+
+        if (progressbarImg.fillAmount == 1)
         {
             SceneManager.LoadScene("Over");
         }
