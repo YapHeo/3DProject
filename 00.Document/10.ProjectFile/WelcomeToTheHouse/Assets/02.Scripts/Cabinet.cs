23c912ba7770a54cceb1ac61e818dd1f177a3abc
Lee bung Gwan <laim4055@gmail.com>
2018-05-10 19:07:22 +0900

    [허지훈]마방진까지

---
+++ 00.Document/10.ProjectFile/WelcomeToTheHouse/Assets/02.Scripts/Cabinet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cabinet : Item
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        base.InteractionId = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
